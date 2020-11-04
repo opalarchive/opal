7cd9d783de9d6dd52df5f23278072244997d3927
Anthony Wang <40553084+cosmicgenius@users.noreply.github.com>
2020-11-03 21:19:42 -0800

    Get rid of unused css files

--- website/src/ProjectView/Project/index.js
+++ website/src/ProjectView/Project/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './index.module.css';
 
 class Project extends React.Component {
   render() {
--- website/src/ProjectView/index.js
+++ website/src/ProjectView/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './index.module.css';
 
 import * as ROUTES from '../Constants/routes';
 
