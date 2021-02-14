f98e12ae8e293f898d346e4d795187837f328874
Anthony Wang <40553084+cosmicgenius@users.noreply.github.com>
2021-02-14 10:20:06 -0800

    chore(new problem): Remove redundant css

--- client/src/ProjectView/Project/View/Pages/NewProblem/index.tsx
+++ client/src/ProjectView/Project/View/Pages/NewProblem/index.tsx
@@ -1,16 +1,27 @@
-import { withStyles, WithStyles, TextField, Slider, Button, Paper } from "@material-ui/core";
+import {
+  withStyles,
+  WithStyles,
+  TextField,
+  Slider,
+  Button,
+  Paper,
+} from "@material-ui/core";
 import React from "react";
 import { CategoryColors, ViewSectionProps } from "../..";
 
-import { Problem as ProblemType, Server, Votes } from "../../../../../../../.shared";
+import {
+  Problem as ProblemType,
+  Server,
+  Votes,
+} from "../../../../../../../.shared";
 import {
   FrontendProblem,
   tryProblemAction,
   newProblem,
   tryProblemActionPrivileged,
 } from "../../../../../Constants/types";
-import TagGroup from "../../Embedded/TagGroup";
-import styles from "./index.css";
+import problemContainerStyles from "../../Embedded/Problem/index.css";
+import detailsStyles from "../../Pages/Details/index.css";
 import { FiChevronLeft, FiPlus } from "react-icons/fi";
 import { compose } from "recompose";
 import { withRouter, RouteComponentProps, Link } from "react-router-dom";
@@ -40,19 +51,25 @@ interface NewProblemState {
   difficulty: number;
 }
 
-class NewProblem extends React.Component<NewProblemProps & WithStyles<typeof styles> & RouteComponentProps<any>, NewProblemState> {
-
+class NewProblem extends React.Component<
+  NewProblemProps &
+    WithStyles<typeof problemContainerStyles> &
+    RouteComponentProps<any>,
+  NewProblemState
+> {
   state = {
     title: "",
     text: "",
     category: "miscellaneous",
     difficulty: 0,
-  }
+  };
 
-  private top = React.createRef<HTMLDivElement>();
-  private prob = React.createRef<HTMLDivElement>();
-
-  constructor(props: NewProblemProps & WithStyles<typeof styles> & RouteComponentProps & ViewSectionProps) {
+  constructor(
+    props: NewProblemProps &
+      WithStyles<typeof problemContainerStyles> &
+      RouteComponentProps &
+      ViewSectionProps
+  ) {
     super(props);
 
     this.newProblem = this.newProblem.bind(this);
@@ -60,9 +77,7 @@ class NewProblem extends React.Component<NewProblemProps & WithStyles<typeof sty
   }
 
   newProblem() {
-    const uid = !!this.props.authUser.uid
-      ? this.props.authUser.uid
-      : "";
+    const uid = !!this.props.authUser.uid ? this.props.authUser.uid : "";
     const { uuid, project } = this.props;
     var { title, text, category, difficulty } = this.state;
     if (title.length == 0) {
@@ -71,11 +86,29 @@ class NewProblem extends React.Component<NewProblemProps & WithStyles<typeof sty
     if (text.length == 0) {
       text = "Empty";
     }
-    if (!["algebra", "geometry", "numberTheory", "combinatorics"].includes(category)) {
+    if (
+      !["algebra", "geometry", "numberTheory", "combinatorics"].includes(
+        category
+      )
+    ) {
       category = "miscellaneous";
     }
-    this.props.newProblem({ author: uid, title, text, category, difficulty, replies: [], votes: {} as Votes, tags: [] });
-    this.props.history.push(ROUTES.PROJECT_PROBLEM.replace(":uuid", uuid).replace(":ind", `${project.problems.length-1}`));
+    this.props.newProblem({
+      author: uid,
+      title,
+      text,
+      category,
+      difficulty,
+      replies: [],
+      votes: {} as Votes,
+      tags: [],
+    });
+    this.props.history.push(
+      ROUTES.PROJECT_PROBLEM.replace(":uuid", uuid).replace(
+        ":ind",
+        `${project.problems.length - 1}`
+      )
+    );
   }
 
   onChange(field: string, value: string | number | number[]) {
@@ -133,44 +166,75 @@ class NewProblem extends React.Component<NewProblemProps & WithStyles<typeof sty
     } = this.props;
 
     return (
-      <div className={classes.root}>
-        <div className={classes.top} ref={this.top}>
-          <Link
-            className={classes.topLink}
-            to={ROUTES.PROJECT_VIEW.replace(":uuid", uuid)}
-          >
-            <FiChevronLeft className={classes.topIcon} />
-            Back
-          </Link>
-          <div className={classes.topFiller} />
-        </div>
-
-        <div ref={this.prob}>
-          <Paper elevation={3} className={classes.probRoot}>
+      <>
+        <div>
+          <Paper elevation={3} className={classes.root}>
             <div className={classes.left}>
-              <div className={classes.leftIndex}>#{project.problems.length+1}</div>
+              <div className={classes.leftIndex}>
+                #{project.problems.length + 1}
+              </div>
             </div>
             <div className={classes.body}>
               <div className={classes.bodyTitle}>
-                <TextField fullWidth value={this.state.title} id="title" label="Title" onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.onChange("title", e.target.value)} />
+                <TextField
+                  fullWidth
+                  value={this.state.title}
+                  id="title"
+                  label="Title"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    this.onChange("title", e.target.value)
+                  }
+                />
               </div>
-              <div className={classes.bodyAuthor}>Proposed by {!!authUser.displayName && authUser.displayName}</div>
+              <div className={classes.bodyAuthor}>
+                Proposed by {!!authUser.displayName && authUser.displayName}
+              </div>
               <div className={classes.bodyText}>
-                <TextField fullWidth variant="filled" value={this.state.text} id="text" multiline label="Text" onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.onChange("text", e.target.value)} />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  value={this.state.text}
+                  id="text"
+                  multiline
+                  label="Text"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    this.onChange("text", e.target.value)
+                  }
+                />
               </div>
               <div className={classes.bodyFiller} />
             </div>
             <div className={classes.right}>
               <div className={classes.rightCategory}>
                 <Dot
-                  color={tupleToRGBString(getCategoryColor(["algebra", "geometry", "combinatorics", "numberTheory"].includes(this.state.category) ? this.state.category : "miscellaneous"))}
+                  color={tupleToRGBString(
+                    getCategoryColor(
+                      [
+                        "algebra",
+                        "geometry",
+                        "combinatorics",
+                        "numberTheory",
+                      ].includes(this.state.category)
+                        ? this.state.category
+                        : "miscellaneous"
+                    )
+                  )}
                   style={{ top: "0.48rem", margin: "0 0.7rem 0 0.2rem" }}
                 />
-                <TextField value={this.state.category} id="category" label="Category" onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.onChange("category", e.target.value)} />
+                <TextField
+                  value={this.state.category}
+                  id="category"
+                  label="Category"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    this.onChange("category", e.target.value)
+                  }
+                />
               </div>
               <div className={classes.rightDifficulty}>
                 <Dot
-                  color={tupleToRGBString(getDifficultyColor(this.state.difficulty))}
+                  color={tupleToRGBString(
+                    getDifficultyColor(this.state.difficulty)
+                  )}
                   style={{ top: "0.48rem", margin: "0 0.7rem 0 0.2rem" }}
                 />
                 <Slider
@@ -186,21 +250,53 @@ class NewProblem extends React.Component<NewProblemProps & WithStyles<typeof sty
                   }}
                   valueLabelDisplay="auto"
                   aria-labelledby="difficulty-header"
-                  getAriaValueText={() =>
-                    `d-${this.state.difficulty}`
-                  }
+                  getAriaValueText={() => `d-${this.state.difficulty}`}
                 />
               </div>
               <div className={classes.rightFiller} />
             </div>
           </Paper>
         </div>
-        <Button variant="contained" color="secondary" onClick={() => this.newProblem()}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => this.newProblem()}
+        >
           <FiPlus /> Add Problem
         </Button>
-      </div>
+      </>
     );
   }
 }
 
-export default compose<NewProblemProps & WithStyles<typeof styles> & RouteComponentProps<any>, NewProblemProps>(withStyles(styles), withRouter)(NewProblem);
+const StyledNewProblem = compose<
+  NewProblemProps &
+    WithStyles<typeof problemContainerStyles> &
+    RouteComponentProps<any>,
+  NewProblemProps
+>(
+  withStyles(problemContainerStyles),
+  withRouter
+)(NewProblem);
+
+const NewProblemWrapper: React.FC<
+  NewProblemProps & WithStyles<typeof detailsStyles>
+> = ({ classes, ...rest }) => {
+  return (
+    <div className={classes.root}>
+      <div className={classes.top}>
+        <Link
+          className={classes.topLink}
+          to={ROUTES.PROJECT_VIEW.replace(":uuid", rest.uuid)}
+        >
+          <FiChevronLeft className={classes.topIcon} />
+          Back
+        </Link>
+        <div className={classes.topFiller} />
+      </div>
+      <StyledNewProblem {...rest} />
+    </div>
+  );
+};
+
+export default withStyles(detailsStyles)(NewProblemWrapper);
